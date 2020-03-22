fcb0e4e2394726c5eb62aa962d8aef56f7527712
macNetCore <61296761+macNetCore@users.noreply.github.com>
2020-03-22 13:43:54 -0700

    Add files via upload

---
+++ Chapter9/HelloCoreRTWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloCoreRTWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello CoreRT World!");
+            Console.ReadKey();
+		}
+	}
+}
