d7c80258367f999f462862fc43a878bccaf232c2
macNetCore <61296761+macNetCore@users.noreply.github.com>
2020-03-22 13:37:45 -0700

    Add files via upload

---
+++ Chapter6/avalonia/basicAvaloniaForm/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace basicAvaloniaForm
+{
+	public class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+			this.FindControl<Button>("Win").Click += delegate
+			{
+				var window = new Window();
+				window.Height = 100;
+				window.Width = 200;
+				window.Content = new TextBlock { Text = "Hello From dialog Window." };
+				window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+				window.ShowDialog(this);
+			};
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+
+		
+	}
+}
---
+++ Chapter6/avalonia/basicAvaloniaForm/Program.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Logging.Serilog;
+
+namespace basicAvaloniaForm
+{
+	class Program
+	{
+		public static void Main(string[] args) => BuildAvaloniaApp().Start(AppMain, args);
+
+		
+		public static AppBuilder BuildAvaloniaApp()
+			=> AppBuilder.Configure<App>()
+				.UsePlatformDetect()
+				.LogToDebug();
+
+		
+		private static void AppMain(Application app, string[] args)
+		{
+			var main = new MainWindow();
+			main.Width = 300;
+			main.Height = 200;
+			app.Run(main);
+		}
+	}
+}
